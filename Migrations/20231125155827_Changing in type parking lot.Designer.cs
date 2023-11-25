c8ede8b6b3bc810d7635eea4049cfe0cac32010e
Pavel Zyryanov <5807keam.hi@gmail.com>
2023-11-25 18:58:49 +0300

    [feat]: update migration

--- Migrations/20231125155827_Changing in type parking lot.Designer.cs
+++ Migrations/20231125155827_Changing in type parking lot.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Urbaton.Data;
@@ -11,9 +12,11 @@ using Urbaton.Data;
 namespace Urbaton.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231125155827_Changing in type parking lot")]
+    partial class Changingintypeparkinglot
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
